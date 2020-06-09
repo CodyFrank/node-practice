5eac6b1679868632a99ca846f0a37f455f194048
Cody Frank <cody.frank30@gmail.com>
2020-06-08 21:58:18 -0400

    practice exporting and importing

---
+++ app.js
@@ -0,0 +1,12 @@
+//  node does note use browser so node does not create a window opject.
+//  instead node wraps all files (refered to as modules) in a function.
+// (function (exports, require, module, __filename, __dirname){
+// module code lives here
+// })
+
+
+// imports say name function. require is given to us via node wrapper
+const sayName = require('./sayName')
+
+// because it is exported in sayName module and imported in this module I can run code from a different module.
+sayName()
---
+++ sayName.js
@@ -0,0 +1,5 @@
+
+// defines function in this file
+const sayName = () => console.log("Hello Cody")
+// exports the above funtion. module.exporst given to us via the node wrapper
+module.exports = sayName
